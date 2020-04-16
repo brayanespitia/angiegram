cb838a7c43b733cea0bec2631b5868a0c8f7ab49
brayan <brayanespitia2013@gmail.com>
2020-04-16 14:20:02 -0500

    marcar mensajes como leidos

--- api/controllers/message.js
+++ api/controllers/message.js
@@ -21,6 +21,7 @@ function saveMessage(req, res) {
   message.receiver = params.receiver;
   message.text = params.text;
   message.created_at = moment().unix();
+  message.viewes = "false";
 
   message.save((err, messageStored) => {
     if (err) return res.status(500).send({ message: "error en la peticion" });
@@ -36,10 +37,10 @@ function getReceivedMessages(req, res) {
   if (req.params.page) {
     page = req.params.page;
   }
-  var itemsPerPage = 4;
+  var itemsPerPage = 3;
 
   Message.find({ receiver: userId })
-    .populate("emitters")
+    .populate("emitter", "name surname image nick _id")
     .paginate(page, itemsPerPage, (err, messages, total) => {
       if (err) return res.status(500).send({ message: "error en la peticion" });
       if (!messages)
@@ -53,8 +54,58 @@ function getReceivedMessages(req, res) {
     });
 }
 
+function getEmmitMessages(req, res) {
+  var userId = req.user.sub;
+  var page = 1;
+  if (req.params.page) {
+    page = req.params.page;
+  }
+  var itemsPerPage = 3;
+
+  Message.find({ emitter: userId })
+    .populate("emitter receiver", "name surname image nick _id")
+    .paginate(page, itemsPerPage, (err, messages, total) => {
+      if (err) return res.status(500).send({ message: "error en la peticion" });
+      if (!messages)
+        return res.status(404).send({ message: "no hay mensajes" });
+
+      return res.status(200).send({
+        total: total,
+        pages: Math.ceil(total / itemsPerPage),
+        messages,
+      });
+    });
+}
+
+function getUnviewedMessages(req, res) {
+  var userId = req.user.sub;
+
+  Message.count({ receiver: userId, viewed: "false" }).exec((err, count) => {
+    if (err) return res.status(500).send({ message: "Error en la petición" });
+    return res.status(200).send({
+      unviewed: count,
+    });
+  });
+}
+
+function setViewedMessages(req, res) {
+  var userId = req.user.sub;
+  Message.update(
+    { receiver: userId, viewed: "false" },
+    { viewed: "true" },
+    { multi: true },
+    (err, messagesUpdated) => {
+      if (err) return res.status(500).send({ message: "Error en la petición" });
+      return res.status(200).send({ messages: messagesUpdated });
+    }
+  );
+}
+
 module.exports = {
   probando,
   saveMessage,
   getReceivedMessages,
+  getEmmitMessages,
+  getUnviewedMessages,
+  setViewedMessages,
 };
--- api/routes/message.js
+++ api/routes/message.js
@@ -9,9 +9,24 @@ var md_auth = require("../middlewares/autenticacion");
 api.get("/probando-md", md_auth.ensureAuth, MessageController.probando);
 api.post("/message", md_auth.ensureAuth, MessageController.saveMessage);
 api.get(
-  "/my-messages",
+  "/my-messages/:page?",
   md_auth.ensureAuth,
   MessageController.getReceivedMessages
+);
+api.get(
+  "/messages/:page?",
+  md_auth.ensureAuth,
+  MessageController.getEmmitMessages
+);
+api.get(
+  "/unviewed-messages",
+  md_auth.ensureAuth,
+  MessageController.getUnviewedMessages
+);
+api.get(
+  "/set-viewed-messages",
+  md_auth.ensureAuth,
+  MessageController.setViewedMessages
 );
 
 module.exports = api;
